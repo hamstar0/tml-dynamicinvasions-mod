94b1da7382632e72524b062c779575c55e47ac85
hamstar0 <webutek@gmail.com>
2019-03-17 03:38:15 -0700

    v0.9.0.1 * Adjusted invasion spawn rate (InvasionSpawnRatePerType) from 1100 to 2000

--- ConfigDefaults.cs
+++ ConfigDefaults.cs
@@ -27,7 +27,7 @@ namespace DynamicInvasions {
 		public int InvasionAddedSizePerStrongPlayer = 40;
 		public int InvasionSpawnRate = 72;
 		public int InvasionSpawnMax = 16;
-		public float InvasionSpawnRatePerType = 1100f;
+		public float InvasionSpawnRatePerType = 2000f;
 
 		public float InvaderLootDropPercentChance = 0.25f;
 
@@ -38,6 +38,12 @@ namespace DynamicInvasions {
 
 		public bool AutoInvasions = true;
 		public int AutoInvasionAverageDays = 9;
+
+
+
+		////
+
+		public const float _0_9_0_InvasionSpawnRatePerType = 1100f;
 
 
 
@@ -52,7 +58,13 @@ namespace DynamicInvasions {
 			if( versSince >= DynamicInvasionsMod.Instance.Version ) {
 				return false;
 			}
-			
+
+			if( versSince <= new Version(0, 9, 0) ) {
+				if( this.InvasionSpawnRatePerType == DynamicInvasionsConfigData._0_9_0_InvasionSpawnRatePerType ) {
+					this.InvasionSpawnRatePerType = newConfig.InvasionSpawnRatePerType;
+				}
+			}
+
 			this.VersionSinceUpdate = DynamicInvasionsMod.Instance.Version.ToString();
 
 			return true;
