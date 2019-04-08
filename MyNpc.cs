41b4db6bf31d7093df06991b005cb5d704234e59
hamstar0 <webutek@gmail.com>
2019-04-07 23:51:16 -0700

    v0.9.0.2 * Added mod compat tweak to solve RecipeBrowser load issues

--- MyNpc.cs
+++ MyNpc.cs
@@ -7,33 +7,40 @@ using Terraria.ModLoader;
 namespace DynamicInvasions {
 	class DynamicInvasionsNpc : GlobalNPC {
 		public override void EditSpawnRate( Player player, ref int spawnRate, ref int maxSpawns ) {
+			if( Main.gameMenu ) { return; }
+
 			var mymod = (DynamicInvasionsMod)this.mod;
 			if( !mymod.ConfigJson.Data.Enabled ) { return; }
-			var modworld = this.mod.GetModWorld<DynamicInvasionsWorld>();
+			var myworld = this.mod.GetModWorld<DynamicInvasionsWorld>();
 
-			if( modworld.Logic.HasInvasionFinishedArriving() && WorldHelpers.IsAboveWorldSurface( player.position ) ) {
+			if( myworld.Logic.HasInvasionFinishedArriving() && WorldHelpers.IsAboveWorldSurface( player.position ) ) {
 				spawnRate = mymod.ConfigJson.Data.InvasionSpawnRate;
 				maxSpawns = mymod.ConfigJson.Data.InvasionSpawnMax;
 			}
 		}
 
 		public override void EditSpawnPool( IDictionary<int, float> pool, NPCSpawnInfo spawnInfo ) {
+			if( Main.gameMenu ) { return; }
+
 			var mymod = (DynamicInvasionsMod)this.mod;
 			if( !mymod.ConfigJson.Data.Enabled ) { return; }
-			var modworld = this.mod.GetModWorld<DynamicInvasionsWorld>();
+			var myworld = this.mod.GetModWorld<DynamicInvasionsWorld>();
 
-			if( modworld.Logic.HasInvasionFinishedArriving() && WorldHelpers.IsAboveWorldSurface( spawnInfo.player.position ) ) {
-				modworld.Logic.EditSpawnPool( pool, spawnInfo );
+			if( myworld.Logic.HasInvasionFinishedArriving() && WorldHelpers.IsAboveWorldSurface( spawnInfo.player.position ) ) {
+				myworld.Logic.EditSpawnPool( pool, spawnInfo );
 			}
 		}
 
 
 		public override bool PreNPCLoot( NPC npc ) {
+			if( Main.gameMenu ) { return base.PreNPCLoot( npc ); }
+
 			var mymod = (DynamicInvasionsMod)this.mod;
 			if( !mymod.ConfigJson.Data.Enabled ) { return base.PreNPCLoot( npc ); }
-			var modworld = this.mod.GetModWorld<DynamicInvasionsWorld>();
 
-			bool hasInvasionArrived = modworld.Logic.HasInvasionFinishedArriving();
+			var myworld = this.mod.GetModWorld<DynamicInvasionsWorld>();
+
+			bool hasInvasionArrived = myworld.Logic.HasInvasionFinishedArriving();
 			bool isAboveSurface = WorldHelpers.IsAboveWorldSurface( npc.position );
 
 			if( hasInvasionArrived && isAboveSurface ) {
@@ -45,6 +52,8 @@ namespace DynamicInvasions {
 
 
 		public override bool CheckDead( NPC npc ) {
+			if( Main.gameMenu ) { return base.CheckDead( npc ); }
+
 			var mymod = (DynamicInvasionsMod)this.mod;
 			if( !mymod.ConfigJson.Data.Enabled ) { return base.CheckDead(npc); }
 			var modworld = this.mod.GetModWorld<DynamicInvasionsWorld>();
